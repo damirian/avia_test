fe51a0e0e8ea4c6b71086ebd914a672d8d9e116c
CharD <maugliwork@gmail.com>
2018-05-13 10:53:59 +0300

    build

--- src/api/ticketsLoader.js
+++ src/api/ticketsLoader.js
@@ -1,12 +1,13 @@
 // export default () => fetch('/data/tickets.json', {mode: 'cors'}).then(data => data.json());
 
-export default () => new Promise((resolve, reject) => {
+export default () =>
+  new Promise((resolve, reject) => {
     setTimeout(async () => {
-        try {
-            const res = await fetch('/data/tickets.json', {mode: 'cors'}).then(data => data.json());
-            resolve(res);
-        } catch (error) {
-            reject(error);
-        }        
-    }, 600);
-})
+      try {
+        const res = await fetch('/data/tickets.json', { mode: 'cors' }).then(data => data.json())
+        resolve(res)
+      } catch (error) {
+        reject(error)
+      }
+    }, 600)
+  })
